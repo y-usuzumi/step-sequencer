0754b62fb8a3641c87ddef24d02da912f0ae9337
Yukio Usuzumi <anohigisavay@gmail.com>
2024-12-19 21:36:47 -0800

    rust-jack quickstart code

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::io;
+
+fn main() {
+    // 1. Create client
+    let (client, _status) =
+        jack::Client::new("rust_jack_simple", jack::ClientOptions::default()).unwrap();
+
+    // 2. Register ports. They will be used in a callback that will be
+    // called when new data is available.
+    let in_a: jack::Port<jack::AudioIn> = client
+        .register_port("二狗_左声道", jack::AudioIn::default())
+        .unwrap();
+    let in_b: jack::Port<jack::AudioIn> = client
+        .register_port("二狗_右声道", jack::AudioIn::default())
+        .unwrap();
+    let mut out_a: jack::Port<jack::AudioOut> = client
+        .register_port("大马猴_左声道", jack::AudioOut::default())
+        .unwrap();
+    let mut out_b: jack::Port<jack::AudioOut> = client
+        .register_port("大马猴_右声道", jack::AudioOut::default())
+        .unwrap();
+    let process_callback = move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
+        let out_a_p = out_a.as_mut_slice(ps);
+        let out_b_p = out_b.as_mut_slice(ps);
+        let in_a_p = in_a.as_slice(ps);
+        let in_b_p = in_b.as_slice(ps);
+        out_a_p.clone_from_slice(in_a_p);
+        out_b_p.clone_from_slice(in_b_p);
+        jack::Control::Continue
+    };
+    let process = jack::contrib::ClosureProcessHandler::new(process_callback);
+
+    // 3. Activate the client, which starts the processing.
+    let active_client = client.activate_async((), process).unwrap();
+
+    // 4. Wait for user input to quit
+    println!("Press enter/return to quit...");
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).ok();
+
+    // 5. Not needed as the async client will cease processing on `drop`.
+    if let Err(err) = active_client.deactivate() {
+        eprintln!("JACK exited with error: {err}");
+    }
+}
