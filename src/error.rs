6f79cdd709a758ab1140e7ae776c4da81403ea78
Yukio Usuzumi <anohigisavay@gmail.com>
2024-12-20 15:51:47 -0800

    Add CoreAudio Rust sinewave generator example

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,8 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum SSError {
+    #[error("CoreAudio error: `{0}`")]
+    CoreAudioError(#[from] coreaudio::Error),
     #[error("Unsupported platform: `{0}`")]
     UnsupportedPlatform(String),
     #[error("Unknown: todo")]
