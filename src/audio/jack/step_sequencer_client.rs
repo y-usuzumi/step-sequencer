6903b1302a23b3126810781d8bebfc7b1ade0304
Yukio Usuzumi <anohigisavay@gmail.com>
2024-12-20 22:53:18 -0800

    Sine wave works in JACK framework

--- src/audio/jack/step_sequencer_client.rs
+++ src/audio/jack/step_sequencer_client.rs
@@ -1,3 +1,5 @@
+use jack::{contrib::ClosureProcessHandler, Frames};
+
 use crate::{audio::SSClient, SSResult};
 use std::io;
 
@@ -15,6 +17,10 @@ impl SSClient for SSJackClient {
         println!("SSJackClient started");
         Ok(())
     }
+}
+
+fn frame_to_time(frames: Frames, sample_rate: usize) -> f64 {
+    frames as f64 / sample_rate as f64
 }
 
 fn create_ss_jack_client() {
@@ -36,9 +42,13 @@ fn create_ss_jack_client() {
     let mut out_b: jack::Port<jack::AudioOut> = client
         .register_port("out_audio_r", jack::AudioOut::default())
         .unwrap();
+    let mut out_sinewave: jack::Port<jack::AudioOut> = client
+        .register_port("out_sinewave", jack::AudioOut::default())
+        .unwrap();
     let mut out_midi: jack::Port<jack::MidiOut> = client
         .register_port("out_midi", jack::MidiOut::default())
         .unwrap();
+    let sample_rate = client.sample_rate();
     let process_callback = move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
         let out_a_p = out_a.as_mut_slice(ps);
         let out_b_p = out_b.as_mut_slice(ps);
@@ -46,6 +56,19 @@ fn create_ss_jack_client() {
         let in_b_p = in_b.as_slice(ps);
         out_a_p.clone_from_slice(in_a_p);
         out_b_p.clone_from_slice(in_b_p);
+
+        // Sine wave
+        let a440 = 220.;
+        let out_sinewave_p = out_sinewave.as_mut_slice(ps);
+        let last_frame_time = ps.last_frame_time();
+        for (idx, v) in out_sinewave_p.iter_mut().enumerate() {
+            let time_elapsed = frame_to_time(last_frame_time + idx as u32, sample_rate);
+            let x = a440 * time_elapsed * 2.0 * std::f64::consts::PI;
+            let y = x.sin();
+            *v = y as f32;
+            println!("Current value: {}", y);
+        }
+
         jack::Control::Continue
     };
     let process = jack::contrib::ClosureProcessHandler::new(process_callback);
