1313183c4ad6f7869b6681a63981dbc7d0a9bde7
Yukio Usuzumi <anohigisavay@gmail.com>
2024-12-25 03:21:44 -0800

    Fix broken JACK

--- step-sequencer/src/audio/jack/step_sequencer_client.rs
+++ step-sequencer/src/audio/jack/step_sequencer_client.rs
@@ -1,8 +1,11 @@
-use crate::{
-    beatmaker::pattern::{create_example_track_hihat, create_example_track_kick_snare},
-    project::Project,
+use std::{
+    sync::mpsc::{self, Sender},
+    thread,
+    time::Duration,
 };
-use jack::{Frames, RawMidi};
+
+use crate::{audio::Command, error::SSError, project::Project};
+use jack::{contrib::ClosureProcessHandler, AsyncClient, Frames, RawMidi};
 use log::{debug, error, info};
 
 use crate::{
@@ -11,31 +14,139 @@ use crate::{
     midi::ChannelVoiceEvent,
     SSResult,
 };
-use std::io;
 
-pub struct SSJackClient {
+pub struct SSJackClient<'a> {
     beatmaker: BeatMaker,
-    project: Project,
+    project: &'a Project,
+    stop_signal_sender: Option<Sender<()>>,
 }
 
-impl SSJackClient {
-    pub fn new(beatmaker: BeatMaker, project: Project) -> Self {
-        Self { beatmaker, project }
+impl<'a> SSJackClient<'a> {
+    pub fn new(beatmaker: BeatMaker, project: &'a Project) -> Self {
+        Self {
+            beatmaker,
+            project,
+            stop_signal_sender: None,
+        }
+    }
+
+    fn create_ss_jack_client(&mut self) {
+        let (stop_signal_sender, stop_signal_receiver) = mpsc::channel();
+        self.stop_signal_sender = Some(stop_signal_sender);
+        let beatmaker_subscription = self.beatmaker.subscribe();
+        self.beatmaker.start(&self.project);
+        thread::spawn(move || -> SSResult<()> {
+            // 1. Create client
+            let (client, _status) =
+                jack::Client::new("Yukio's Step Sequencer", jack::ClientOptions::default())
+                    .unwrap();
+
+            // 2. Register ports. They will be used in a callback that will be
+            // called when new data is available.
+            let in_a: jack::Port<jack::AudioIn> = client
+                .register_port("in_audio_l", jack::AudioIn::default())
+                .unwrap();
+            let in_b: jack::Port<jack::AudioIn> = client
+                .register_port("in_audio_r", jack::AudioIn::default())
+                .unwrap();
+            let mut out_a: jack::Port<jack::AudioOut> = client
+                .register_port("out_audio_l", jack::AudioOut::default())
+                .unwrap();
+            let mut out_b: jack::Port<jack::AudioOut> = client
+                .register_port("out_audio_r", jack::AudioOut::default())
+                .unwrap();
+            let mut out_sinewave: jack::Port<jack::AudioOut> = client
+                .register_port("out_sinewave", jack::AudioOut::default())
+                .unwrap();
+            let mut out_midi: jack::Port<jack::MidiOut> = client
+                .register_port("out_midi", jack::MidiOut::default())
+                .unwrap();
+            // thread::sleep(Duration::from_secs(100));
+            let process_callback = move |state: &mut TestState,
+                                         client: &jack::Client,
+                                         process_scope: &jack::ProcessScope|
+                  -> jack::Control {
+                let out_a_p = out_a.as_mut_slice(process_scope);
+                let out_b_p = out_b.as_mut_slice(process_scope);
+                let in_a_p = in_a.as_slice(process_scope);
+                let in_b_p = in_b.as_slice(process_scope);
+                out_a_p.clone_from_slice(in_a_p);
+                out_b_p.clone_from_slice(in_b_p);
+
+                // Sine wave test
+                process_sine_wave(client, &mut out_sinewave, process_scope);
+
+                // Midi test
+                // let _ = process_midi(state, client, &mut out_midi, process_scope);
+                let _ = process_beatmaker(
+                    &beatmaker_subscription,
+                    state,
+                    client,
+                    &mut out_midi,
+                    process_scope,
+                );
+
+                let frame_time = process_scope.last_frame_time();
+                let frames = process_scope.n_frames();
+                let sample_rate = client.sample_rate() as u32;
+                let seconds = (frame_time + frames) / sample_rate;
+                if seconds > state.last_event_midi_seconds {
+                    state.last_event_midi_seconds = seconds;
+                }
+
+                jack::Control::Continue
+            };
+            let process = jack::contrib::ClosureProcessHandler::with_state(
+                TestState {
+                    last_event_midi_seconds: 0,
+                },
+                process_callback,
+                move |_, _, _| jack::Control::Continue,
+            );
+            // 3. Activate the client, which starts the processing.
+            // Must bind it to a variable, otherwise the client is deactivated on drop immediately!
+            let active_client = client.activate_async((), process).unwrap();
+            if let Ok(_) = stop_signal_receiver.recv() {
+                Ok(())
+            } else {
+                Err(crate::error::SSError::Unknown(
+                    "stop_signal_sender disconnected while receiving from stop_signal_receiver"
+                        .to_string(),
+                ))
+            }
+        });
     }
 }
 
-impl SSClient for SSJackClient {
+impl<'a> SSClient for SSJackClient<'a> {
     fn start(&mut self) -> SSResult<()> {
-        create_ss_jack_client();
+        self.create_ss_jack_client();
         info!("SSJackClient started");
         Ok(())
     }
 
     fn stop(&mut self) -> SSResult<()> {
-        todo!("See code below: active_client.deactivate()");
+        // 5. Not needed as the async client will cease processing on `drop`.
+        // if let Err(err) = active_client.deactivate() {
+        //     error!("JACK exited with error: {err}");
+        // }
+        if let Some(ref stop_signal_sender) = self.stop_signal_sender {
+            stop_signal_sender.send(());
+        }
+        Ok(())
     }
 
-    fn send_command(&self, command: &str) -> SSResult<()> {}
+    fn send_command(&self, command: Command) -> SSResult<()> {
+        // TODO: Copy-paste from CoreAudio side. Improve the design
+        match command {
+            Command::ChangeTempo(tempo) => {
+                let project_settings = self.project.project_settings();
+                project_settings.write().unwrap().tempo = tempo;
+            }
+        }
+
+        Ok(())
+    }
 }
 
 struct TestState {
@@ -126,98 +237,4 @@ fn process_beatmaker(
         midi_writer.write(&raw_midi)?;
     }
     Ok(())
-}
-
-fn create_ss_jack_client() {
-    // 1. Create client
-    let (client, _status) =
-        jack::Client::new("Yukio's Step Sequencer", jack::ClientOptions::default()).unwrap();
-
-    // 2. Register ports. They will be used in a callback that will be
-    // called when new data is available.
-    let in_a: jack::Port<jack::AudioIn> = client
-        .register_port("in_audio_l", jack::AudioIn::default())
-        .unwrap();
-    let in_b: jack::Port<jack::AudioIn> = client
-        .register_port("in_audio_r", jack::AudioIn::default())
-        .unwrap();
-    let mut out_a: jack::Port<jack::AudioOut> = client
-        .register_port("out_audio_l", jack::AudioOut::default())
-        .unwrap();
-    let mut out_b: jack::Port<jack::AudioOut> = client
-        .register_port("out_audio_r", jack::AudioOut::default())
-        .unwrap();
-    let mut out_sinewave: jack::Port<jack::AudioOut> = client
-        .register_port("out_sinewave", jack::AudioOut::default())
-        .unwrap();
-    let mut out_midi: jack::Port<jack::MidiOut> = client
-        .register_port("out_midi", jack::MidiOut::default())
-        .unwrap();
-
-    let mut beatmaker = BeatMaker::default();
-    let beatmaker_subscription = beatmaker.subscribe();
-
-    let process_callback = move |state: &mut TestState,
-                                 client: &jack::Client,
-                                 process_scope: &jack::ProcessScope|
-          -> jack::Control {
-        let out_a_p = out_a.as_mut_slice(process_scope);
-        let out_b_p = out_b.as_mut_slice(process_scope);
-        let in_a_p = in_a.as_slice(process_scope);
-        let in_b_p = in_b.as_slice(process_scope);
-        out_a_p.clone_from_slice(in_a_p);
-        out_b_p.clone_from_slice(in_b_p);
-
-        // Sine wave test
-        process_sine_wave(client, &mut out_sinewave, process_scope);
-
-        // Midi test
-        // let _ = process_midi(state, client, &mut out_midi, process_scope);
-        let _ = process_beatmaker(
-            &beatmaker_subscription,
-            state,
-            client,
-            &mut out_midi,
-            process_scope,
-        );
-
-        let frame_time = process_scope.last_frame_time();
-        let frames = process_scope.n_frames();
-        let sample_rate = client.sample_rate() as u32;
-        let seconds = (frame_time + frames) / sample_rate;
-        if seconds > state.last_event_midi_seconds {
-            state.last_event_midi_seconds = seconds;
-        }
-
-        jack::Control::Continue
-    };
-    let process = jack::contrib::ClosureProcessHandler::with_state(
-        TestState {
-            last_event_midi_seconds: 0,
-        },
-        process_callback,
-        move |_, _, _| jack::Control::Continue,
-    );
-
-    // let _ = beatmaker.start_with_beat_note_map(BEAT_NOTE_MAP_BITWIG);
-    let project = Project::new(&beatmaker);
-    for track in EXAMPLE_DRUMTRACKS_BITWIG.all_tracks() {
-        project.add_track(track);
-    }
-    let tracks = project.tracks();
-
-    let _ = beatmaker.start(&project);
-    info!("BeatMaker started");
-    info!(
-        "Current tempo: {}",
-        project.project_settings().read().unwrap().tempo
-    );
-
-    // 3. Activate the client, which starts the processing.
-    let active_client = client.activate_async((), process).unwrap();
-
-    // 5. Not needed as the async client will cease processing on `drop`.
-    if let Err(err) = active_client.deactivate() {
-        error!("JACK exited with error: {err}");
-    }
 }
