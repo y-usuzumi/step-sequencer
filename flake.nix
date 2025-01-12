f82441d9328409decd3a2dff01ecec621dfc1a9b
Yukio Usuzumi <anohigisavay@gmail.com>
2025-01-12 05:04:41 -0800

    Initial GUI commit.

--- flake.nix
+++ flake.nix
@@ -41,12 +41,40 @@
           packages = with pkgs; [
             rustToolchain
             openssl
-            libjack2
             pkg-config
             cargo-deny
             cargo-edit
             cargo-watch
             rust-analyzer
+            # GUI
+            nodejs
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+          ];
+
+          # https://tauri.app/start/prerequisites/#linux
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gobject-introspection
+            cargo
+            cargo-tauri
+            nodejs
+          ];
+
+          buildInputs = with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+            libjack2
           ];
 
           env = {
